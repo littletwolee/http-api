d49a372e0ab40582ba1f7eafd4246520f6b46e72
littletwolee <904693492@qq.com>
2016-05-04 17:33:51 +0800

    add auth

--- mod_http_user.erl
+++ mod_http_user.erl
@@ -11,7 +11,7 @@
 
 -behaviour(gen_mod).
 
--export([start/2, stop/1, process/2, check_permissions/2]).
+-export([start/2, stop/1, process/2, check_permissions/1]).
 
 -include("ejabberd.hrl").
 -include("jlib.hrl").
@@ -27,8 +27,8 @@ start(_Host, _Opts) ->
 stop(_Host) ->
     ok.
 
--spec check_permissions(binary(), []) -> boolean().
-check_permissions(Version, Headers) ->
+-spec check_permissions([]) -> boolean().
+check_permissions(Headers) ->
     Authorization = case lists:keyfind('Authorization', 1, Headers) of
 			{_, A} -> case is_binary(A) of
 				      true -> binary_to_list(A);
@@ -50,65 +50,61 @@ check_permissions(Version, Headers) ->
 				    end;
 			  false -> false
 		      end,
+    Version = case lists:keyfind(<<"Version">>, 1, Headers) of
+			  {_, V} -> case is_binary(V) of
+					true -> binary_to_list(V);
+					false -> V
+				    end;
+			  false -> false
+		      end,
     if 
-	Authorization == false; Token == false; EncryptionToken == false -> false;
-	%% ture -> case mod_versionrule:checktoken(Version, Authorization, Token) of
-	%% 	    err ->false;
-	%% 	    Other -> ok
-	%% 	end
+	Authorization == false; Token == false; EncryptionToken == false ;Version == false-> false;
 	true -> mod_versionrule:checktoken(Version, Authorization, Token, EncryptionToken)
     end.
 process(_, #request{method = 'POST', 
 		    path = [ <<"api">>, <<"user">>, <<"register">> ], 
+		    headers = Headers,
 		    data = Data}) ->
-    {List} = jiffy:decode(Data),
-    {_, Name} = lists:keyfind(<<"name">>, 1, List),
-    {_, Pwd} = lists:keyfind(<<"pwd">>, 1, List),
-    Url = "http://localhost:8080/api/user/create",
-    SendData = jiffy:encode({[{<<"name">>, Name},{<<"pwd">>, Pwd}]}),
-    case tools:http_post(post, Url, "application/json", SendData) of
-	{{_, 200, "OK"}, _, Result} ->
-	    {ResultList} = jiffy:decode(Result),
-	    {_, ObjectId} = lists:keyfind(<<"ObjectId">>, 1, ResultList),
-	    if ObjectId /= "" ->
-		    io:format("~p",[ObjectId]),
-	    	    case mod_redis:set_kv(#kv{key = ObjectId, value = Pwd}) of
-	    		ok -> 
-	    		    tools:json_response(200, [jiffy:encode({[{state, <<"ok">>}]})]);
-	    		err ->
-	    		    TranUrl = binary_to_list(list_to_bitstring(["http://localhost:8080/api/user/delete/",ObjectId])),
-	    		    tools:http_get(delete, TranUrl),
-	    		    tools:json_response(200, [jiffy:encode({[{state, <<"err">>}]})])
-	    	    end
+    case check_permissions(Headers) of
+	true ->
+	    {List} = jiffy:decode(Data),
+	    {_, Name} = lists:keyfind(<<"name">>, 1, List),
+	    {_, Pwd} = lists:keyfind(<<"pwd">>, 1, List),
+	    Url = "http://localhost:8080/api/user/create",
+	    SendData = jiffy:encode({[{<<"name">>, Name},{<<"pwd">>, Pwd}]}),
+	    case tools:http_post(post, Url, "application/json", SendData) of
+		{{_, 200, "OK"}, _, Result} ->
+		    {ResultList} = jiffy:decode(Result),
+		    {_, ObjectId} = lists:keyfind(<<"ObjectId">>, 1, ResultList),
+		    if ObjectId /= "" ->
+			    io:format("~p",[ObjectId]),
+			    case mod_redis:set_kv(#kv{key = ObjectId, value = Pwd}) of
+				ok -> 
+				    tools:json_response(200, [jiffy:encode({[{state, <<"ok">>}]})]);
+				err ->
+				    TranUrl = binary_to_list(list_to_bitstring(["http://localhost:8080/api/user/delete/",ObjectId])),
+				    tools:http_get(delete, TranUrl),
+				    tools:json_response(200, [jiffy:encode({[{state, <<"err">>}]})])
+			    end
+		    end;
+		_ ->
+		    tools:json_response(200, [jiffy:encode({[{state, <<"err">>}]})])
 	    end;
-    %%mod_redis:set_token(#token{key = Name, value = Pwd}),
-	_ ->
-	    tools:json_response(200, [jiffy:encode({[{state, <<"err">>}]})])
+	false ->
+	    tools:json_response(401)
     end;
-
-%% process(_, #request{method = 'POST', 
-%% 		   path = [ <<"api">>, <<"user">>, <<"login">>]}) ->
-%%     {List} = jiffy:decode(Data),
-%%     {_, Name} = lists:keyfind(<<"name">>, 1, List),
-%%     {_, Pwd} = lists:keyfind(<<"pwd">>, 1, List),
-%%     {_, Token} = lists:keyfind(<<"token">>, 1, List),
-    
-%%     tools:json_response(200, [jiffy:encode({[{state, <<"err">>}]})]);
 process(_, #request{method = 'GET', 
 		     path = [ <<"api">>, <<"user">>, <<"getuserbyid">> ], 
-		     q = [{<<"objectId">>, ObjectId}, {<<"token">>, Token}, {<<"usertoken">>, UserToken}, {<<"encryptiontoken">>, EncryptionToken}]}) ->
-    Url = binary_to_list(list_to_bitstring(["http://localhost:8080/api/user/id/",ObjectId])),
-    Result = tools:http_get(get, Url),
-    %% Case httpc:request(get,{Url, [], [], []},[],[]) of   
-    %%     {ok, {_, _, Result}}-> Result;  
-    %%     {error, {_, _, Result}}->io:format("error cause ~p~n",[Result])  
-    %% end,
-%    Time = "1460015905",
-%    Salt = "83B214E255765F948B0ACD6954229D7B",
-%    RangeNum = [[7, 15, 23, 31], [10, 15, 23, 31]],
-%    HashSalt = re:replaced(string:substr(Salt, 7, 2)),
-%    Token = tools:hash_sha256_string(list_to_bitstring([tools:hash_sha256_string(Time),Salt])),
-    tools:json_response(200, Result);
+		     q = [{<<"objectId">>, ObjectId}]}) ->
+    case check_permissions(Headers) of
+	true ->
+	    Url = binary_to_list(list_to_bitstring(["http://localhost:8080/api/user/id/",ObjectId])),
+	    Result = tools:http_get(get, Url),
+	    tools:json_response(200, Result);
+	false ->
+	    tools:json_response(401)
+    end;
+    
 process(_, #request{method = 'GET',
 		    path = [ <<"api">>, <<"user">>, <<"test">> ],
 		    auth = HTTPAuth, headers = Headers} = Req) ->
@@ -142,31 +138,14 @@ process(_, #request{method = 'GET',
 process(_, #request{method = 'DELETE', 
 		    path = [ <<"api">>, <<"user">>, <<"delete">> ], 
 		    q = [{_ , ObjectId}]}) ->
-    Url = binary_to_list(list_to_bitstring(["http://localhost:8080/api/user/delete/",ObjectId])),
-    Result = tools:http_get(delete, Url),
-    tools:json_response(200, [Result]);
+    case check_permissions(Headers) of
+	true ->
+	    Url = binary_to_list(list_to_bitstring(["http://localhost:8080/api/user/delete/",ObjectId])),
+	    Result = tools:http_get(delete, Url),
+	    tools:json_response(200, [Result]);
+	false ->
+	    tools:json_response(401)
+    end;
 process(_, Req) ->
     io:format("~p", [Req]),
     tools:json_response(404, "").
-
-%splicesalt(RangeNum, Num, Str, Salt) ->
-%    if
-%	Str == "" ->
-%	    StrOld = string:substr(Salt, 0, Num -1);
-%        Str /= "" ->
-%            StrOld = Str
-%    end,
-%    if
-%	string:len(Str) == 32 ->
-%	    Str;
-%	true ->
-%	    [RanList, RepList] = RangeNum,
-%	    this:splicesalt(RangeNum, Num + 1, list_to_binary([StrOld, 
-%							  lists:nth(Num, RepList), 
-%							  string:substr(Salt, 
-%									lists:nth(Num, RanList) + 2, 
-%									string:len(Salt) - lists:nth(Num, RanList) + 2)
-%							 ]))
-%    end.
-
-    
